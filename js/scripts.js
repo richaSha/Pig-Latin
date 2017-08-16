1e458344ebe910ba278ef0a4350a75a39ce7055c
Guest User <Guest@epicodus-41.local>
2017-08-16 16:37:52 -0700

    added

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,80 @@
-//user interface
-
 //business logic
 var pigLatin = function(inputString){
   var vowels = ['a','A','i','I','o','O','u','U','e','E','y','Y'];
   var data;
   var updatedString;
-  var CCNo;
+  var isFirstLetterVowel;
   var stringIndex =0;
+  var consonantArray =[];
+  var arrayList = inputString.split("");
+
+  var firstWord = arrayList[stringIndex];
+
   var vowelFinder = function(data,index) {
-    if(index == 0 && (vowel == 'y' || vowel == 'Y' )){
+    if(index == 0 && (data == 'y' || data == 'Y' )){
       return false;
     }
     else {
-      vowels.map(function(vowel){
-        if(vowel == data){
-          return true;
-        }else {
-          return false;
-        }
-      })
+      if(data.match(/[a i o u e]/g)){
+        return true;
+      }else {
+        return false;
+      }
     }
   }
 
+  var consonantArray = function(){
+    arrayList.push(arrayList[stringIndex])
+    delete arrayList[stringIndex]
 
-
-
-  if(data.match([/a-z A-Z/g])){
-    var arr = inputString.split("");
-      var vFinder = vowelFinder(arr[stringIndex],stringIndex);
-      if(vFinder){
-        updatedString = inputString + "way";
-      }else{
-        updatedString = (inputString.shift()).push(arr[stringIndex]);
-        for(var i=1; i<inputString.length;i++ ){
-
-          var vowel = vowelFinder(inputString.charAt(i), i);
-          if(vowel){
-            if(inputString.charAt(i) == 'u'|| inputString.charAt(i) == 'U'){
-              updatedString = (inputString.shift()).push(arr[stringIndex]);
-            }else{
-              updatedString = (inputString.shift()).push(arr[stringIndex]);
-            }
-          }
+    firstWord = arrayList[stringIndex+1];
+    isFirstLetterVowel = vowelFinder(firstWord, 2)
+    if(isFirstLetterVowel){
+      arrayList.push('ay');
+      updatedString = (arrayList.toString()).replace(/,/g, '');
+    }else{
+        stringIndex += 1;
+        if(stringIndex < inputString.length){
+          consonantArray(arrayList);
+        }else{
+          arrayList.push('ay');
+          updatedString = (arrayList.toString()).replace(/,/g, '');
         }
+    }
+  }
 
+  if(firstWord.match(/[a-z A-Z]/g)){
+    isFirstLetterVowel = vowelFinder(firstWord, stringIndex);
+    if(isFirstLetterVowel){
+      updatedString = inputString + "way";
+    }
+    else {
+      if(firstWord == 'q'){
+        var secondWord = arrayList[stringIndex+1];
+        if(secondWord == 'u'){
+          arrayList.push(arrayList[stringIndex])
+          arrayList.push(arrayList[stringIndex+1])
+          delete arrayList[stringIndex]
+          delete arrayList[stringIndex+1]
+          arrayList.push('ay');
+          updatedString = (arrayList.toString()).replace(/,/g, '');
+        }
+      }else {
+        consonantArray();
       }
 
+    }
   }else{
     updatedString = inputString;
-
   }
   return updatedString;
-
 }
+
+//user interface
+$(document).ready(function() {
+  $('button').click(function() {
+    var inputString = $('input').val();
+    var newString = pigLatin(inputString);
+    $('.result').text(newString);
+  })
+})
